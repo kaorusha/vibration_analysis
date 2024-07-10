2d813f62af3631f3da19266f6d3d3f3877827d71
kaorusha <tobewithhunters@gmail.com>
2024-07-10 16:44:18 +0800

    add data aquisition and add FG signal for RPM measurement

--- analog_input.py
+++ analog_input.py
@@ -1,15 +1,22 @@
 import nidaqmx
 from nidaqmx.constants import AcquisitionType, READ_ALL_AVAILABLE
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
 with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_accel_chan("cDAQ1Mod1/ai0", "acc0")
-    task.ai_channels.add_ai_accel_chan("cDAQ1Mod1/ai1", "acc1")
-    task.ai_channels.add_ai_accel_chan("cDAQ1Mod1/ai2", "acc2")
-    task.timing.cfg_samp_clk_timing(48000, sample_mode=AcquisitionType.FINITE, samps_per_chan=10)
+    task.ai_channels.add_ai_accel_chan("cDAQ1Mod1/ai0", "acc0", sensitivity=10.22)
+    task.ai_channels.add_ai_accel_chan("cDAQ1Mod1/ai1", "acc1", sensitivity=10.11)
+    task.ai_channels.add_ai_accel_chan("cDAQ1Mod1/ai2", "acc2", sensitivity=10.28)
+    task.ai_channels.add_ai_accel_chan("cDAQ1Mod1/ai3", "fg", sensitivity=200)
+    fs = 48000
+    acq_time = 10
+    task.timing.cfg_samp_clk_timing(fs, sample_mode=AcquisitionType.FINITE, samps_per_chan=fs*acq_time)
+    t1 = time.perf_counter()
     data = task.read(READ_ALL_AVAILABLE)
-    plt.plot(data[0])
-    plt.plot(data[1])
-    plt.plot(data[2])
-    plt.ylabel('Amplitude')
-    plt.title('Waveform')
+    t2 = time.perf_counter()
+    print('acquire time: {}'.format(t2 - t1))
+    df = pd.DataFrame({'up':data[0], 'down':data[1], 'axial':data[2], 'fg':data[3]}, index=pd.Series(np.arange(0, acq_time, 1/fs)))
+    df.plot(title='acc data', xlabel='time(sec)', ylabel='g')
+    df.to_excel('../002577_ud.xlsx', sheet_name='acc_data')
     plt.show()
