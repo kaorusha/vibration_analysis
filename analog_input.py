cb8f1af4358063e0e34bab8ae606f4f3b8f68bc5
kaorusha <tobewithhunters@gmail.com>
2024-08-20 13:06:25 +0800

    change fg sensitivity and output file naming

--- analog_input.py
+++ analog_input.py
@@ -9,7 +9,7 @@ with nidaqmx.Task() as task:
     task.ai_channels.add_ai_accel_chan("cDAQ1Mod1/ai1", "acc1", sensitivity=10.11)
     task.ai_channels.add_ai_accel_chan("cDAQ1Mod1/ai2", "acc2", sensitivity=10.28)
     #task.ci_channels.add_ci_freq_chan use this https://github.com/ni/nidaqmx-python/blob/master/examples/counter_in/read_freq.py
-    task.ai_channels.add_ai_accel_chan("cDAQ1Mod1/ai3", "fg", sensitivity=200)
+    task.ai_channels.add_ai_accel_chan("cDAQ1Mod1/ai3", "fg", sensitivity=600)
     fs = 51200
     acq_time = 10
     task.timing.cfg_samp_clk_timing(fs, sample_mode=AcquisitionType.FINITE, samps_per_chan=fs*acq_time)
@@ -17,8 +17,8 @@ with nidaqmx.Task() as task:
     data = task.read(READ_ALL_AVAILABLE)
     t2 = time.perf_counter()
     print('acquire time: {}'.format(t2 - t1))
-    file_name = '002577_ud'
-    df = pd.DataFrame({file_name+'up':data[0], file_name+'down':data[1], file_name+'axial':data[2], file_name+'fg':data[3]})
+    file_name = '001833_ud'
+    df = pd.DataFrame({file_name+'_up':data[0], file_name+'_down':data[1], file_name+'_axial':data[2], file_name+'fg':data[3]})
     df.plot(title='acc data', xlabel='time(sec)', ylabel='g')
     df.to_excel('../%s.xlsx'%file_name, sheet_name='acc_data', index=False)
     plt.show()
